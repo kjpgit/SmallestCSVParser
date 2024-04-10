755f26b0d4ec3da9f0866c553077eabdecd30198
kjpgit <kjpgit@users.noreply.github.com>
2024-04-10 18:31:18 -0500

    add example of removeEnclosingQuotes

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,8 +1,14 @@
 using var sr = new StreamReader("test1.csv");
 var parser = new SmallestCSV.SmallestCSVParser(sr);
 
+// Set this to true if you don't care about preserving the quotes around fields.
+// (Sometimes that is used to distinguish a null vs empty field.)
+// You also can remove the quotes for any field(s) yourself, it doesn't have to
+// be done by the parser.
+const bool removeEnclosingQuotes=false;
+
 while (true) {
-    List<string>? columns = parser.ReadNextRow();
+    List<string>? columns = parser.ReadNextRow(removeEnclosingQuotes:removeEnclosingQuotes);
     if (columns == null) {
         Console.WriteLine("End of file reached");
         break;
