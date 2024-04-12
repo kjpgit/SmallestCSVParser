2a16790ca50562521af433145276b2e84796f58d
kjpgit <kjpgit@users.noreply.github.com>
2024-04-12 12:44:51 -0500

    tests: add more tests

--- SmallestCSVParserTests/UnitTest1.cs
+++ SmallestCSVParserTests/UnitTest1.cs
@@ -21,18 +21,8 @@ public class SmallestCSVParserTest
                 " 3"],
         ];
 
-        for (int i = 0; i < expected.Length; i++) {
-            var columns = parser.ReadNextRow();
-            Assert.IsNotNull(columns);
-            printRow(i, columns);
-            Assert.AreEqual(expected[i].Length, columns.Count(), "count mismatch");
-            for (var j = 0; j < columns.Count(); j++) {
-                Assert.AreEqual(expected[i][j], columns[j], "row mismatch");
-            }
-        }
-        foreach (var i in Enumerable.Range(0, 3)) {
-            Assert.IsNull(parser.ReadNextRow());
-        }
+        var rows = readAllRows(() => parser.ReadNextRow()).ToList();
+        compareRows(expected: expected, actual: rows);
     }
 
     [TestMethod]
@@ -51,18 +41,8 @@ public class SmallestCSVParserTest
                 " 3"],
         ];
 
-        for (int i = 0; i < expected.Length; i++) {
-            var columns = parser.ReadNextRow(removeEnclosingQuotes:false);
-            Assert.IsNotNull(columns);
-            printRow(i, columns);
-            Assert.AreEqual(expected[i].Length, columns.Count(), "count mismatch");
-            for (var j = 0; j < columns.Count(); j++) {
-                Assert.AreEqual(expected[i][j], columns[j], "row mismatch");
-            }
-        }
-        foreach (var i in Enumerable.Range(0, 3)) {
-            Assert.IsNull(parser.ReadNextRow());
-        }
+        var rows = readAllRows(() => parser.ReadNextRow(removeEnclosingQuotes:false)).ToList();
+        compareRows(expected: expected, actual: rows);
     }
 
     [TestMethod]
@@ -87,14 +67,63 @@ public class SmallestCSVParserTest
     public void TestEOFAfterField() {
         string data = "abc,this_is_terminated_by_eof";
         using var sr = streamReaderFromString(data);
+        var parser = new SmallestCSVParser(sr);
+
+        string[][] expected = [ [ "abc", "this_is_terminated_by_eof" ] ];
+
+        var rows = readAllRows(() => parser.ReadNextRow()).ToList();
+        compareRows(expected: expected, actual: rows);
+    }
+
+    [TestMethod]
+    public void TestConformingEmbeddedWhitespace() {
+        string data = "\"all whitespace\r is\n legal\r\n in a quoted\t\v field\"";
+        using var sr = streamReaderFromString(data);
 
         var parser = new SmallestCSVParser(sr);
         var columns = parser.ReadNextRow();
         Assert.IsNotNull(columns);
-        Assert.AreEqual(2, columns.Count());
-        Assert.AreEqual("abc", columns[0]);
-        Assert.AreEqual("this_is_terminated_by_eof", columns[1]);
+        Assert.AreEqual(1, columns.Count());
+        Assert.AreEqual(data[1..^1], columns[0]);
         Assert.IsNull(parser.ReadNextRow());
+
+        using var sr2 = streamReaderFromString(data);
+        parser = new SmallestCSVParser(sr2);
+        columns = parser.ReadNextRow(removeEnclosingQuotes:false);
+        Assert.IsNotNull(columns);
+        Assert.AreEqual(1, columns.Count());
+        Assert.AreEqual(data, columns[0]);
+        Assert.IsNull(parser.ReadNextRow());
+    }
+
+    [TestMethod]
+    public void TestNonConformingEmbeddedWhitespace() {
+        // '\r' and '\n' are (supposedly) not allowed in a non-quoted field.
+        // If we see them, we treat them as equivalent to '\r\n'.  Most likely,
+        // the csv was just written with unix ('\n') line endings, not windows ('\r\n').
+        string data = "Bad1\rBad2\nOK1\r\n";
+        using var sr = streamReaderFromString(data);
+        var parser = new SmallestCSVParser(sr);
+
+        // This is 3 rows
+        string[][] expected = [ [ "Bad1" ], [ "Bad2" ], [ "OK1" ] ];
+
+        var rows = readAllRows(() => parser.ReadNextRow()).ToList();
+        compareRows(expected: expected, actual: rows);
+    }
+
+    private IEnumerable<List<string>> readAllRows(Func<List<string>?> action) {
+        while (true) {
+            var row = action();
+            if (row == null) {
+                break;
+            }
+            printRow(row);
+            yield return row;
+        }
+        // Make sure more calls still return null...
+        Assert.IsNull(action());
+        Assert.IsNull(action());
     }
 
     private StreamReader streamReaderFromString(string data) {
@@ -103,8 +132,18 @@ public class SmallestCSVParserTest
         return new StreamReader(mem);
     }
 
-    private void printRow(int rowNum, List<string> columns) {
-        Console.WriteLine($"-- Read row {rowNum}");
+    private void compareRows(string[][] expected, List<List<string>> actual) {
+        Assert.AreEqual(expected.Length, actual.Count(), "row count mismatch");
+        for (int i = 0; i < expected.Length; i++) {
+            Assert.AreEqual(expected[i].Length, actual[i].Count(), "column count mismatch");
+            for (var j = 0; j < expected[i].Length; j++) {
+                Assert.AreEqual(expected[i][j], actual[i][j], "column data mismatch");
+            }
+        }
+    }
+
+    private void printRow(List<string> columns) {
+        Console.WriteLine($"-- Read row");
         for (var i = 0; i < columns.Count(); i++) {
             Console.WriteLine($"column {escapeText(columns[i])}");
         }
